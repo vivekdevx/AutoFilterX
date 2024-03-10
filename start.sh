295893d92bac398d2ac31d105ebcdf791e768391
Abu Talha Ansari <101631188+GamerBhai02@users.noreply.github.com>
2024-03-10 08:26:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GamerBhai02/AllMoviesLinkBot.git /AllMoviesLinkBot
+  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AllMoviesLinkBot
+  git clone $UPSTREAM_REPO /TheMovieProviderBot
 fi
-cd /AllMoviesLinkBot
+cd /TheMovieProviderBot
 pip3 install -U -r requirements.txt
-echo "Starting AllMoviesLinkBot...."
+echo "Starting TheMovieProviderBot...."
 python3 bot.py
