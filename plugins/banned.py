0899571eec2a380aa06bdd8abf942bfb1ae81734
Abu Talha Ansari <101631188+GamerBhai02@users.noreply.github.com>
2024-06-10 18:59:29 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -26,7 +26,7 @@ async def ban_reply(bot, message):
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('Support', url=f'https://t.me/+pp_D21tjCtAzMjc1')
+        InlineKeyboardButton('Support/Admin', url=f'https://t.me/GamerBhai02')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
