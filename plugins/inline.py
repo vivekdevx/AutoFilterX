bdb9c4de197e45f1ccc857023511354c87bad013
Abu Talha Ansari <101631188+GamerBhai02@users.noreply.github.com>
2025-02-28 09:49:23 +0530

    Update inline.py
    Font Updated

--- plugins/inline.py
+++ plugins/inline.py
@@ -107,7 +107,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton('𝖲𝖾𝖺𝗋𝖼𝗁 𝖺𝗀𝖺𝗂𝗇', switch_inline_query_current_chat=query)
         ]
         ]
     return InlineKeyboardMarkup(buttons)
